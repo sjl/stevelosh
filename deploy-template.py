75b05de65c5f519a5741660bdf674842ab00e79c
Steve Losh <steve@stevelosh.com>
2009-01-15 18:06:54 -0500

    Added a template for deploy.py.

---
+++ deploy-template.py
@@ -0,0 +1,17 @@
+# Rename this to deploy.py when you've filled in everything.
+
+DATABASE_ENGINE = ''
+DATABASE_NAME = ''
+DATABASE_USER = ''
+DATABASE_PASSWORD = ''
+DATABASE_HOST = ''
+DATABASE_PORT = ''
+
+DEBUG = False
+
+# The longer the better.  Django automatically generates one for you when you 
+# start a new project.
+SECRET_KEY = ''
+
+# Make sure it ends in a trailing slash! 
+BASE_DIR = '/full/path/to/project/directory/'
