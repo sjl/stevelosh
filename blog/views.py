a4ab241b029351811c38858be4d911e8d88669e3
Steve Losh <steve@stevelosh.com>
2009-02-12 14:13:06 -0500

    The blog now uses Djangos paginator.

--- blog/views.py
+++ blog/views.py
@@ -3,11 +3,11 @@ from markdown import markdown
 from django.shortcuts import get_object_or_404, render_to_response
 from django.http import HttpResponseRedirect, HttpResponsePermanentRedirect
 from django.core.urlresolvers import reverse
+from django.core.paginator import Paginator
 from akismet import Akismet
 from stevelosh import deploy
 
 ak = Akismet(deploy.AKISMET_API_KEY, blog_url='http://stevelosh.com/')
-ENTRIES_PER_PAGE = 10
 
 def entry(request, year, month, day, slug):
     entry = get_object_or_404(Entry, slug=slug, pub_date__year=year, 
@@ -21,16 +21,16 @@ def old_entry(request, year, month, day, slug):
     return HttpResponsePermanentRedirect(reverse('blog-entry',
                                          args=(year, month, day, slug)))
 
-def list(request, page=0):
+def list(request, page=1):
     page = int(page)
-    start_index = page * ENTRIES_PER_PAGE
-    end_index = start_index + ENTRIES_PER_PAGE
-    entries = Entry.objects.all().order_by('-pub_date')
-    entries = entries.filter(published=True)[start_index:end_index]
+    
+    entries = Entry.objects.all().filter(published=True).order_by('-pub_date')
+    p = Paginator(entries, 7, orphans=4).page(page)
+    
     return render_to_response('blog/list.html', 
-        { 'entries': entries,
-          'older_page': page+1 if end_index < Entry.objects.count() else None,
-          'newer_page': page-1 if page != 0 else None } )
+        { 'entries': p.object_list,
+          'older_page': p.next_page_number() if p.has_next() else None,
+          'newer_page': p.previous_page_number() if p.has_previous() else None } )
 
 def comment(request):
     fields = request.POST
