36e02992ac22e9eaadd92ba34766c9068c3e188c
Steve Losh <steve@stevelosh.com>
2009-02-12 13:56:47 -0500

    Refactored Thoughts to use Djangos built-in pagination.

--- thoughts/views.py
+++ thoughts/views.py
@@ -1,23 +1,23 @@
 from stevelosh.thoughts.models import TextThought, LinkThought
 from django.shortcuts import render_to_response
+from django.core.paginator import Paginator
 import operator
 
 ENTRIES_PER_PAGE = 10
 
-def list(request, page=0):
+def list(request, page=1):
     page = int(page)
-    start_index = page * ENTRIES_PER_PAGE
-    end_index = start_index + ENTRIES_PER_PAGE
-    total_count = TextThought.objects.count() + LinkThought.objects.count()
     
     thoughts = []
     thoughts += TextThought.objects.all().order_by('-posted')
     thoughts += LinkThought.objects.all().order_by('-posted')
     thoughts.sort(key=operator.attrgetter('posted'))
     thoughts.reverse()
-    thoughts = thoughts[start_index:end_index]
+    
+    paginator = Paginator(thoughts, 5, orphans=2)
+    p = paginator.page(page)
     
     return render_to_response('thoughts/list.html', 
-        { 'thoughts': thoughts,
-          'older_page': page+1 if end_index < total_count else None,
-          'newer_page': page-1 if page != 0 else None } )
+        { 'thoughts': p.object_list,
+          'older_page': p.next_page_number() if p.has_next() else None,
+          'newer_page': p.previous_page_number() if p.has_previous() else None } )
