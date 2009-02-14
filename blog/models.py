ca12e2885418925d0ec8dbffdc0168005974b4f0
Steve Losh <steve@stevelosh.com>
2009-02-14 11:46:36 -0500

    Fixed the plural for blog entries too.

--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,10 @@ class Entry(models.Model):
     body = models.TextField()
     published = models.BooleanField(default=False)
     
+    class Meta:
+        verbose_name = 'entry'
+        verbose_name_plural = 'entries'
+    
     @models.permalink
     def get_absolute_url(self):
         return ('blog-entry', (self.pub_date.year, self.pub_date.month, 
