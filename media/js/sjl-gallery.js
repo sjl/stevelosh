4d48f4a5bf8f8d0d4a23ddda7d847f6c73b383ca
Steve Losh <steve@stevelosh.com>
2010-01-06 21:17:38 -0500

    Get the images working, except for the gallery.

--- media/js/sjl-gallery.js
+++ media/js/sjl-gallery.js
@@ -6,21 +6,22 @@ $(function() {
         
         var cols = 4;
         var padding = 10;
-        var total_width = $(this).width();
-        var width = (total_width / cols) - 2 * padding;
+        var total_width = $(this).width() - (cols * 2 * padding);
+        var width = (total_width / cols);
+        
         $(this).find('img').css({width: width, margin: padding});
         
         $(this).before('<div class="gallery-pane"></div>');
         $(this).find('img:first').clone()
                                  .addClass('gallery-display')
-                                 .css({width: '', margin: ''})
+                                 .css({width: '', margin: '0px auto'})
                                  .appendTo('div.gallery-pane');
     });
-
+    
     $('div.gallery img').click(function() {
         var new_image = $(this).clone()
                                .addClass('gallery-display')
-                               .css({width: '', margin: '', display: 'none'});
+                               .css({width: '', margin: '0px auto', display: 'none'});
         $('img.gallery-display').fadeOut('fast', function() {
             $(this).remove();
             new_image.appendTo('div.gallery-pane').fadeIn('slow');
