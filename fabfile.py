3657e7f48500344c7f65538fabc531072b771477
Steve Losh <steve@stevelosh.com>
2010-01-09 02:18:15 -0500

    Add a publish task to the fabfile.

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,11 @@
 from fabric.api import *
+import os
+import fabric.contrib.project as project
+
+PROD = 'sjl.webfactional.com'
+DEST_PATH = '/home/sjl/webapps/slc/'
+ROOT_PATH = os.path.abspath(os.path.dirname(__file__))
+DEPLOY_PATH = os.path.join(ROOT_PATH, 'deploy')
 
 def clean():
     local('rm -rf ./deploy')
@@ -16,3 +23,12 @@ def reserve():
 
 def smush():
     local('smusher ./media/images')
+
+@hosts(PROD)
+def publish():
+    regen()
+    project.rsync_project(
+        remote_dir=DEST_PATH,
+        local_dir=DEPLOY_PATH.rstrip('/') + '/',
+        delete=True
+    )
